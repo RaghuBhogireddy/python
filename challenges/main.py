c25abe8a618c489cc7694cc168be56300393be18
raghuram bhogireddy <raghurambhogireddy@gmail.com>
2022-06-27 21:46:13 +0530

    Added Code

---
+++ challenges/main.py
@@ -0,0 +1,7 @@
+
+def list_benefits():
+    return "More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"
+
+print(list_benefits())
+print(type(list_benefits()))
+print(dir(list))
