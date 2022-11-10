734dafdbdb5d87c0208072bcca6a07722e7113a6
raghubhogireddy <raghurambhogireddy@gmail.com>
2022-11-10 22:19:57 +0530

    day1 : modified check Binary program

--- practice_challenges/checkForBinary.py
+++ practice_challenges/checkForBinary.py
@@ -1,15 +1,10 @@
 def main(str):
-    flag = 0
-    #binary = [i for i in str]
     for i in str:
-        if int(i) == 0 or int(i) == 1:
-            pass
-        else :
-            flag = 1
+        if int(i) != 0 and int(i) != 1:
+            return 0
         
-    if flag == 1 : return 0
-    else: return 1
+    return 1
 
 
 if __name__ == "__main__":
-    print(main("10000100010"))
+    print(main("34315"))
